f86764f73a652e8e376c13a3e250d56f85d007b2
RedDragon-Code <r.alberto.p.sanchez@gmail.com>
2021-12-10 22:57:51 -0500

    hasta aqui llego el repaso

--- BackEnd/BackEndApp/src/controllers/index.ts
+++ BackEnd/BackEndApp/src/controllers/index.ts
@@ -1,2 +1,4 @@
 export * from './ping.controller';
 export * from './pedido-persona.controller';
+export * from './persona.controller';
+export * from './producto.controller';
--- BackEnd/BackEndApp/src/models/persona.model.ts
+++ BackEnd/BackEndApp/src/models/persona.model.ts
@@ -35,7 +35,7 @@ export class Persona extends Entity {
 
   @property({
     type: 'string',
-    required: true,
+    required: false,
   })
   clave: string;
 
